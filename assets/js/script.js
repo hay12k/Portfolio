e3d8985f16c95bddf26ba99d4ddb2dea23d1ae41
hay12k <hayaangroup93@gmail.com>
2023-03-14 22:24:18 +0300

    modified:   assets/css/style.css 	modified:   assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
+// ------background visibale When scroll down-----------
 const header = document.querySelector("header");
 window.addEventListener ("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// -------project popup----------
+document.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("project-bottom")){
+        toggleProjectPopup();
+    }
+});
+function toggleProjectPopup(){
+    document.querySelector(".project-popup").classList.toggle("open");
+}
