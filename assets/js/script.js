4751786ec9d201bb89a2b6e0abf21c7dbc4d6138
hay12k <hayaangroup93@gmail.com>
2023-03-16 16:16:50 +0300

    modified:   assets/css/style.css 	modified:   assets/js/script.js 	modified:   index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,33 @@
+// ------Active navlist-----------
+var btnContainer = document.getElementById("navlist");
+var li = btnContainer.getElementsByClassName("li");
+
+for (var i = 0; i < li.length; i++) {
+    li[i].addEventListener('click', function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace("active");
+        this.className += "active";
+    })
+}
 // ------background visibale When scroll down-----------
 const header = document.querySelector("header");
-window.addEventListener ("scroll", function(){
+window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 // -------project popup----------
-document.addEventListener("click", (e) =>{
-    if(e.target.classList.contains("project-bottom")){
+document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("project-bottom")) {
         toggleProjectPopup();
         projectItemDetails(e.target.parentElement);
     }
 });
-function toggleProjectPopup(){
+function toggleProjectPopup() {
     document.querySelector(".project-popup").classList.toggle("open");
 }
 document.querySelector(".project-popup-close").addEventListener("click", toggleProjectPopup)
 
-function projectItemDetails(projectItem){
+function projectItemDetails(projectItem) {
     document.querySelector(".pp-thumbnail img").src = projectItem.querySelector(".project-img").src;
     document.querySelector(".project-popup-subtitle span").innerHTML = projectItem.querySelector(".project-title").innerHTML;
     document.querySelector(".project-popup-body").innerHTML = projectItem.querySelector(".project-item-details").innerHTML;
