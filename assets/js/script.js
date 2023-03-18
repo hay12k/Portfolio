4f7a3d3dff057b6fac8f0aba62e7a15af16d275b
hay12k <hayaangroup93@gmail.com>
2023-03-18 07:20:45 +0300

    modified:   assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,11 @@ let navlist = document.querySelector('.navlist');
 menu.onclick = ()=>{
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('active');
-}
+};
+window.onscroll = ()=>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+};
 
 // ------Active navlist-----------
 
