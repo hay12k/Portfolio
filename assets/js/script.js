d05f885fb793316d0de9ec593ac06763593a08f7
hay12k <hayaangroup93@gmail.com>
2023-03-23 15:51:41 +0300

    modified:   assets/css/style.css 	modified:   assets/js/script.js 	modified:   index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,15 @@ window.onscroll = ()=>{
 };
 
 // ------Active navlist-----------
-
+var btnContainer = document.getElementById("navlist");
+var li = btnContainer.getElementsByClassName("li");
+for(var i = 0; i < li.length; i++){
+    li[i].addEventListener('click', function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active");
+        this.className += " active";
+    })
+}
 // ------background visibale When scroll down-----------
 const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
